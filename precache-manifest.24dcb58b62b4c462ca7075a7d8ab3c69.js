89d22632ebd23b4b194aca995aa7c8e1211c996b
ij-monster <53199154+ij-monster@users.noreply.github.com>
2019-07-23 06:39:30 +0700

    + csv-exporter

---
+++ precache-manifest.24dcb58b62b4c462ca7075a7d8ab3c69.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1555d6c06a7eded9d7",
+    "url": "./static/js/main.dc1555d6.chunk.js"
+  },
+  {
+    "revision": "ce5137f6d7cbbfe44831",
+    "url": "./static/js/1.ce5137f6.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3b16368fec63712caf6dcb093dbc9a05",
+    "url": "./index.html"
+  }
+];
